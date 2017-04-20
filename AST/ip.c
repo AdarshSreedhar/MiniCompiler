8d7334e5d4c3f022a11310bb8aca2eda9641df06
AdarshSreedhar <addu121@gmail.com>
2017-04-20 19:14:59 +0530

    Add files via upload

---
+++ AST/ip.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int a;
+    int b;
+    if(a>b)
+    {   
+        if(a==2)
+        {
+            int a;
+            a=a-b;
+        }
+    }    
+    else
+    {
+        a=a+10;
+    }
+}
